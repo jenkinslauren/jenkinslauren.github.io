0e9e9ce441584d50a9c2eaa90d79bb076ef2bbd4
Lauren Jenkins <lauren.jenkins@my.wheaton.edu>
2026-01-28 14:00:07 -0500

    toggle

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".toggle-button");
-  const menu = document.querySelector(".menu");
+  
+  const toggle = document.querySelector(".navbar-burger, .toggle-button");
+  const menu = document.querySelector(".navbar-menu, .menu");
 
   if (toggle && menu) {
     toggle.addEventListener("click", () => {
-      menu.classList.toggle("active");
+      // Toggle the active class
+      toggle.classList.toggle("is-active");
+      menu.classList.toggle("is-active");
     });
   }
 });
